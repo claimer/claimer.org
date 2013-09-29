017f98c7316caf938b893d7efbb4bb6fb245bfad
foxydemon <foxydemon@gmail.com>
2013-09-29 19:26:29 +0200

    Remove configuration check

--- apps/frontend/config/frontendConfiguration.class.php
+++ apps/frontend/config/frontendConfiguration.class.php
@@ -4,9 +4,5 @@ class frontendConfiguration extends sfApplicationConfiguration
 {
   public function initialize()
   {
-    if (! in_array(sfConfig::get('app_claimer_default_currency'), array('EUR', 'USD')))
-    {
-      throw new sfException(sprintf("The %s default currency in app.yml is invalid.", sfConfig::get('app_claimer_default_currency')));
-    }
   }
 }
